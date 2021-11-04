f6886041a60e0fdf38c854b3e26fe60b323cc5ae
CrumpetsNTea <t.botell@gmail.com>
2021-11-03 20:19:05 -0400

    can't beleive that actually worked

--- fetcher.js
+++ fetcher.js
@@ -9,18 +9,18 @@ let URL = slicedInput[0];
 const request = require('request');
 const fs = require('fs');
 
+let content = '';
+
 request(URL, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+  content += error;
+  content += response && response.statusCode;
+  content += body;
+  fs.writeFile('/Users/tobias/lighthouse/w2/d3/page-fetcher/file.txt', content, err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    //file written successfully
+  });
+  
 });
-
-
-fs.writeFile('/Users/tobias/lighthouse/w2/d3/page-fetcher/file.txt', content, err => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  //file written successfully
-});
-
