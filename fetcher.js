633efea49997badd71c3640a18892b1fc49b5ec9
CrumpetsNTea <t.botell@gmail.com>
2021-11-03 21:39:45 -0400

    working Y/N answer for overwrite if file already exists. Otherwise if file does not exist then executes as normal and creates new file named what was passed in

--- fetcher.js
+++ fetcher.js
@@ -4,6 +4,13 @@
 
 const request = require('request');
 const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 
 const slicedInput = process.argv.slice(2);
 
@@ -14,24 +21,56 @@ let filePath = slicedInput[1];
 let content = '';
 
 fs.access(filePath, fs.R_OK, err => { //checks if file exists by checking if it has permissions
-  if (!err) { //if it doesn't throw an error, it means that it already exists because only an existing file can have permissions
-    console.log("File exists");
-    return;
-  }//otherwise it continues on and writes the results of the code below into the passed in file (which will create a new file named what is passed in)
-  request(URL, (error, response, body) => {
-    content += error; //if error, pushes it to file
-    content += response && response.statusCode; //pushes to content string
-    content += body; //pushes to content string
-    fs.writeFile(filePath, content, err => { //after all info has been added to variable content, it is written to designated file
-      if (err) {
-        console.error(err);
-        return; //if there is an error it will console.log it and abort
-  
+  if (!err) { //if it does not throw an error, it means that it already exists because only an existing file can have permissions
+    rl.question(`File already exists. Would you like to overwrite the file? Y/N:`, (answer) => {
+      if (answer.trim() === `N`) { //if they type no
+        console.log("Okay, try again with a new file path");
+        process.exit();
       }
-  
-      let stats = fs.statSync(filePath); //gets the stats of the file once it's been written to
-      let fileSizeInBytes = stats["size"]; //gets the size of the file
-      console.log(`Downloaded and saved ${fileSizeInBytes} to ${filePath}`); //console.logs final message saying size and file path
+      if (answer.trim() === `Y`) { //if they want to overwrite then executes fetch
+        console.log("Getting file information");
+        request(URL, (error, response, body) => {
+          content += error; //if error, pushes it to file
+          content += response && response.statusCode; //pushes to content string
+          content += body; //pushes to content string
+          fs.writeFile(filePath, content, err => { //after all info has been added to variable content, it is written to designated file
+            if (err) {
+              console.error(err);
+              return; //if there is an error it will console.log it and abort
+            }
+      
+            let stats = fs.statSync(filePath); //gets the stats of the file once it's been written to
+            let fileSizeInBytes = stats["size"]; //gets the size of the file
+            console.log(`Downloaded and saved ${fileSizeInBytes} to ${filePath}`); //console.logs final message saying size and file path
+            process.exit(); //exits process once it is done otherwise readline stays open
+          });
+    
+        });
+      }
     });
-  });
+  }
+  if (err) {
+    //otherwise if the file does not exist it will write the results of the code below into the passed in file (which will create a new file named what is passed in)
+    console.log("Getting file information");
+    request(URL, (error, response, body) => {
+      content += error; //if error, pushes it to file
+      content += response && response.statusCode; //pushes to content string
+      content += body; //pushes to content string
+      fs.writeFile(filePath, content, err => { //after all info has been added to variable content, it is written to designated file
+        if (err) {
+          console.error(err);
+          return; //if there is an error it will console.log it and abort
+        }
+  
+        let stats = fs.statSync(filePath); //gets the stats of the file once it's been written to
+        let fileSizeInBytes = stats["size"]; //gets the size of the file
+        console.log(`Downloaded and saved ${fileSizeInBytes} to ${filePath}`); //console.logs final message saying size and file path
+        process.exit(); //exits process once it is done otherwise readline stays open
+      });
+
+    });
+
+  }
+
 });
+
