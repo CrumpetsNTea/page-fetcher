47a9fc5a238f5dc8026af0b690c37b2aadeb510f
CrumpetsNTea <t.botell@gmail.com>
2021-11-03 21:44:00 -0400

    refactor. assigned function fetch() which is then called later in the program if user types Y to overwrite or passed in file does not exist

--- fetcher.js
+++ fetcher.js
@@ -20,6 +20,28 @@ let filePath = slicedInput[1];
 
 let content = '';
 
+const fetch = () => { //define a function that will execute our fetch code
+
+  console.log("Getting file information");
+  request(URL, (error, response, body) => {
+    content += error; //if error, pushes it to file
+    content += response && response.statusCode; //pushes to content string
+    content += body; //pushes to content string
+    fs.writeFile(filePath, content, err => { //after all info has been added to variable content, it is written to designated file
+      if (err) {
+        console.error(err);
+        return; //if there is an error it will console.log it and abort
+      }
+
+      let stats = fs.statSync(filePath); //gets the stats of the file once it's been written to
+      let fileSizeInBytes = stats["size"]; //gets the size of the file
+      console.log(`Downloaded and saved ${fileSizeInBytes} to ${filePath}`); //console.logs final message saying size and file path
+      process.exit(); //exits process once it is done otherwise readline stays open
+    });
+
+  });
+};
+
 fs.access(filePath, fs.R_OK, err => { //checks if file exists by checking if it has permissions
   if (!err) { //if it does not throw an error, it means that it already exists because only an existing file can have permissions
     rl.question(`File already exists. Would you like to overwrite the file? Y/N:`, (answer) => {
@@ -28,49 +50,12 @@ fs.access(filePath, fs.R_OK, err => { //checks if file exists by checking if it 
         process.exit();
       }
       if (answer.trim() === `Y`) { //if they want to overwrite then executes fetch
-        console.log("Getting file information");
-        request(URL, (error, response, body) => {
-          content += error; //if error, pushes it to file
-          content += response && response.statusCode; //pushes to content string
-          content += body; //pushes to content string
-          fs.writeFile(filePath, content, err => { //after all info has been added to variable content, it is written to designated file
-            if (err) {
-              console.error(err);
-              return; //if there is an error it will console.log it and abort
-            }
-      
-            let stats = fs.statSync(filePath); //gets the stats of the file once it's been written to
-            let fileSizeInBytes = stats["size"]; //gets the size of the file
-            console.log(`Downloaded and saved ${fileSizeInBytes} to ${filePath}`); //console.logs final message saying size and file path
-            process.exit(); //exits process once it is done otherwise readline stays open
-          });
-    
-        });
+        fetch();
       }
     });
   }
-  if (err) {
-    //otherwise if the file does not exist it will write the results of the code below into the passed in file (which will create a new file named what is passed in)
-    console.log("Getting file information");
-    request(URL, (error, response, body) => {
-      content += error; //if error, pushes it to file
-      content += response && response.statusCode; //pushes to content string
-      content += body; //pushes to content string
-      fs.writeFile(filePath, content, err => { //after all info has been added to variable content, it is written to designated file
-        if (err) {
-          console.error(err);
-          return; //if there is an error it will console.log it and abort
-        }
-  
-        let stats = fs.statSync(filePath); //gets the stats of the file once it's been written to
-        let fileSizeInBytes = stats["size"]; //gets the size of the file
-        console.log(`Downloaded and saved ${fileSizeInBytes} to ${filePath}`); //console.logs final message saying size and file path
-        process.exit(); //exits process once it is done otherwise readline stays open
-      });
-
-    });
-
+  if (err) { //if file does not exist then executes fetch
+    fetch();
   }
-
 });
 
