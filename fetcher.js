015296b5954cb34759ed924022361ff84685aac9
CrumpetsNTea <t.botell@gmail.com>
2021-11-03 21:47:23 -0400

    added other conditionals that covers whether the user puts in variations of yes or no when asked to overwrite

--- fetcher.js
+++ fetcher.js
@@ -45,12 +45,12 @@ const fetch = () => { //define a function that will execute our fetch code
 fs.access(filePath, fs.R_OK, err => { //checks if file exists by checking if it has permissions
   if (!err) { //if it does not throw an error, it means that it already exists because only an existing file can have permissions
     rl.question(`File already exists. Would you like to overwrite the file? Y/N:`, (answer) => {
-      if (answer.trim() === `N`) { //if they type no
-        console.log("Okay, try again with a new file path");
-        process.exit();
+      if (answer.trim() === `N` || answer.trim() === `n` || answer.trim() === `no` || answer.trim() === `No`) {
+        console.log("Okay, try again with a new file path");  //if they don't want to overwrite then tells them to try again
+        process.exit(); //exits the process
       }
-      if (answer.trim() === `Y`) { //if they want to overwrite then executes fetch
-        fetch();
+      if (answer.trim() === `Y` || answer.trim() === `y` || answer.trim() === `yes` || answer.trim() === `Yes`) {
+        fetch(); //if they want to overwrite then executes fetch
       }
     });
   }
