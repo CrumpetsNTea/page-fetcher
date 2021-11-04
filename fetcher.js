1a1ee4347581b9cd2da12899b07313e28543d472
CrumpetsNTea <t.botell@gmail.com>
2021-11-03 21:01:16 -0400

    add some comments and add the start of the if file exists functionality

--- fetcher.js
+++ fetcher.js
@@ -11,26 +11,27 @@ let URL = slicedInput[0];
 
 let filePath = slicedInput[1];
 
-
 let content = '';
 
-request(URL, (error, response, body) => {
-  content += error;
-  content += response && response.statusCode;
-  content += body;
-  fs.writeFile(filePath, content, err => {
-    if (err) {
-      console.error(err);
-      return;
-
-    }
-    let stats = fs.statSync(filePath);
-    let fileSizeInBytes = stats["size"];
-    console.log(`Downloaded and saved ${fileSizeInBytes} to ${filePath}`);
-  });
+fs.access(filePath, fs.R_OK, err => { //checks if file exists by checking if it has permissions
+  if (!err) { //if it doesn't throw an error, it means that it already exists because only an existing file can have permissions
+    console.log("File exists");
+    return;
+  }//otherwise it continues on and writes the results of the code below into the passed in file (which will create a new file named what is passed in)
+  request(URL, (error, response, body) => {
+    content += error; //if error, pushes it to file
+    content += response && response.statusCode; //pushes to content string
+    content += body; //pushes to content string
+    fs.writeFile(filePath, content, err => { //after all info has been added to variable content, it is written to designated file
+      if (err) {
+        console.error(err);
+        return; //if there is an error it will console.log it and abort
   
+      }
+  
+      let stats = fs.statSync(filePath); //gets the stats of the file once it's been written to
+      let fileSizeInBytes = stats["size"]; //gets the size of the file
+      console.log(`Downloaded and saved ${fileSizeInBytes} to ${filePath}`); //console.logs final message saying size and file path
+    });
+  });
 });
-
-// var fs = require("fs"); //Load the filesystem module
-// var stats = fs.stat("file.txt")
-// var fileSizeInBytes = stats["size"]
